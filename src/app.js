65709a9de4f7f4864c6eea5d57cd1cd803d76a9a
SharpRoyalz <116153953+SharpRoyalz@users.noreply.github.com>
2023-09-06 12:09:14 +0800

    Added an email

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+
 const express = require("express");
 const path = require("path");
 const mustacheExpress = require("mustache-express");
 const JOBS = require("./jobs");
 
-require("dotenv").config();
-const nodemailer = require("nodemailer");
-const bodyParser = require("body-parser");
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Configure mustache
+app.set("views", path.join(__dirname, "pages"));
+app.set("view engine", "mustache");
+app.engine("mustache", mustacheExpress());
+
+// Render the template
+app.get("/", (req, res) => {
+    res.render("index", { jobs: JOBS });
+});
+
+app.get("/jobs/:id", (req, res) => {
+    const id = req.params.id;
+    const matchedJob = JOBS.find((job) => job.id.toString() === id);
+    res.render("job", { job: matchedJob });
+});
 
 const transporter = nodemailer.createTransport({
     host: "mail.gmx.com",
@@ -49,27 +67,9 @@ app.post("/jobs/:id/apply", (req, res) => {
             res.status(500).send("Error sending email");
         } else {
             console.log("Email sent: " + info.response);
-            res.status(200).send("Email sent successfully");
+            res.status(200).render("applied");
         }
     });
-});
-
-app.use(express.static(path.join(__dirname, "public")));
-
-// Configure mustache
-app.set("views", path.join(__dirname, "pages"));
-app.set("view engine", "mustache");
-app.engine("mustache", mustacheExpress());
-
-// Render the template
-app.get("/", (req, res) => {
-    res.render("index", { jobs: JOBS });
-});
-
-app.get("/jobs/:id", (req, res) => {
-    const id = req.params.id;
-    const matchedJob = JOBS.find((job) => job.id.toString() === id);
-    res.render("job", { job: matchedJob });
 });
 
 const port = process.env.PORT || 3000;
